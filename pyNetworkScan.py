57421417be4526eb945dc910e3e364c0aa8837ac
s4d0l1n <129994936+s4d0l1n@users.noreply.github.com>
2023-06-27 17:28:56 -0700

    Update pyNetworkScan.py

--- pyNetworkScan.py
+++ pyNetworkScan.py
@@ -5,10 +5,18 @@ from collections import OrderedDict
 import pygraphviz as pgv
 import socket
 
+thisip=""
+allhops = []
 def read_xml(file_name):
     with open(file_name) as file:
         return xmltodict.parse(file.read())
 
+def getMyIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    myip = s.getsockname()[0]
+    s.close()
+    return myip
 
 def get_scanned_hosts(scan_dict, net=""):
     scanned_hosts = {}
@@ -19,7 +27,7 @@ def get_scanned_hosts(scan_dict, net=""):
 
         open_ports = get_open_ports(ports)
 
-        scanned_hosts[address] = {"theports": open_ports, "parentIP": ""}
+        scanned_hosts[address] = {"theports": open_ports, "parentIP": [thisip]}
 
     return scanned_hosts
 
@@ -38,16 +46,41 @@ def get_open_ports(ports):
 
 def perform_traceroute(scanned_hosts):
     for host in scanned_hosts.keys():
-        result, unans = traceroute(host, maxttl=6, verbose=None)
+        result, unans = traceroute(host, maxttl=6, verbose=True)
+        hops=[]
+        
+        print(f"LENGTH OF ANASERED PACKETS {len(unans)}")
+        pprint.pprint(result.get_trace().items())
+        for trace in result.get_trace().values():
+            
+            
+            for sub_trace in trace.values():
+                    
+                r = list(sub_trace)
+                #s, t = zip(*r)
+                hops.append(str(r[0]))
+                print(f"the hops: {hops}")
 
-        prevhop = [
-            keyb
-            for trace in result.get_trace().values()
-            for sub_trace in trace.values()
-            for keyb in sub_trace if type(keyb) != bool
-            ]
-        #pprint.pprint(prevhop)
-        scanned_hosts[host]["parentIP"] = prevhop
+                if str(r[1]) == "True":
+                    lastHop = hops[-1]
+                    lastHop = lastHop[:lastHop.rfind(".")] + ".1"
+                    print(f"last hop is: {lastHop}")
+                    if len(hops) > 1 : hops.insert(-1, lastHop)
+                    hops.insert(0, thisip)
+                    scanned_hosts[host]["parentIP"] = list(dict.fromkeys(hops))
+                    allhops.append(list(dict.fromkeys(hops)))
+                    print(scanned_hosts[host]["parentIP"])
+                    print(f"added path: {hops}")
+
+
+        # prevhop = [
+        #     keyb
+        #     for trace in result.get_trace().values()
+        #     for sub_trace in trace.values()
+        #     for keyb in sub_trace if type(keyb) != bool
+        #     ]
+        # #pprint.pprint(prevhop)
+        # scanned_hosts[host]["parentIP"] = prevhop
 
 
 def create_network_graph(scanned_hosts, filename="all"):
@@ -55,18 +88,18 @@ def create_network_graph(scanned_hosts, filename="all"):
 
     for host, data in scanned_hosts.items():
         parent_node = list(data["parentIP"])
-        lastHop = parent_node[-1]
+        # lastHop = parent_node[-1]
         
-        print("*********************************************************************************")
-        lastHop = lastHop[:lastHop.rfind(".")] + ".0"
-        # print(lastHop[:lastHop.rfind(".")] + ".0")
-        print(lastHop)
-        print("*********************************************************************************")
-        if len(parent_node) > 1 : parent_node.insert(-1,lastHop) 
+        # print("*********************************************************************************")
+        # lastHop = lastHop[:lastHop.rfind(".")] + ".0"
+        # # print(lastHop[:lastHop.rfind(".")] + ".0")
+        # print(lastHop)
+        #print("*********************************************************************************")
+        # if len(parent_node) > 1 : parent_node.insert(-1,lastHop) 
         #else: 
         #     parent_node.insert(0,lastHop) 
         #parent_node.insert(-1,lastHop)
-        pprint.pprint(parent_node)
+        #pprint.pprint(parent_node)
         #edges = list(zip(parent_node, parent_node[1:]))
 
 
@@ -83,7 +116,7 @@ def create_network_graph(scanned_hosts, filename="all"):
 
 if __name__ == '__main__':
     scan_dict = read_xml('outputscan.xml')
-    
+    thisip= getMyIP()
     scanned_hosts = get_scanned_hosts(scan_dict)
     uniqueNets = set([ips[:ips.rfind(".")] for ips in scanned_hosts])
     perform_traceroute(scanned_hosts)
@@ -97,3 +130,4 @@ if __name__ == '__main__':
 
     
     create_network_graph(scanned_hosts)
+    pprint.pprint(allhops)
