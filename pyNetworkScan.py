ed886ce3c516ef810750f26388de5d87373a9ca6
s4d0l1n <129994936+s4d0l1n@users.noreply.github.com>
2023-07-03 03:21:39 -0700

    Update pyNetworkScan.py

--- pyNetworkScan.py
+++ pyNetworkScan.py
@@ -7,6 +7,7 @@ import pygraphviz as pgv
 import socket
 import numpy as np
 
+
 thisip=""
 allhops = []
 hostsAndHops = dict()
@@ -28,9 +29,9 @@ def get_scanned_hosts(scan_dict, net=""):
         address = host['address']['@addr'] if type(host['address']) is not list else host['address'][0]['@addr']
         ports = host['ports']['port'] if "port" in host['ports'] else []
 
-        open_ports = list(get_open_ports(ports))
+        open_ports = get_open_ports(ports)
 
-        scanned_hosts[address] = {"theports": open_ports, "parentIP": [thisip]}
+        scanned_hosts[address] = {"theports": open_ports, "parentIP": None}
 
     return scanned_hosts
 
@@ -44,13 +45,13 @@ def get_open_ports(ports):
     else:
         open_ports[ports['@portid']] = ports['service']['@name']
 
-    return open_ports.items()
+    return open_ports
 
 
 def perform_traceroute(scanned_hosts):
     for host in scanned_hosts.keys():
         print(f"tracerouting {host}:")
-        ans, unans = sr(IP(dst=host, ttl=(1,6))/ICMP(), timeout=5, verbose=0, retry=2)
+        ans, unans = sr(IP(dst=host, ttl=(1,6))/ICMP(), timeout=2, verbose=0, retry=2)
         hops=[]
         ans.summary( lambda s, r : hops.append(str(r.sprintf("%IP.src%"))))
         hops = list(dict.fromkeys(hops))
@@ -70,18 +71,23 @@ def get_random_hex_color():
     
 
 def create_network_graph(scanned_hosts, filename="all"):
-    graph = pgv.AGraph(overlap=False, splines="curved", directed=True, rankdir="TB", strict=False, beautify=True)
+    graph = pgv.AGraph(overlap=False, splines="ortho", directed=True, rankdir="TB", strict=False)
    
 
     for host, data in scanned_hosts.items():
         parent_node = data["parentIP"]
         maxHops = len(data["parentIP"])
 
-        formatedPorts = f"<BR /> {[ p + '<BR />' for p in data['theports']]}"
-        graph.add_node(host, label=f"<IP: {host} \n Ports: {formatedPorts}>", shape='rectangle', rank=f"{host[:host.rfind('.')]}")
+        formatedPorts = {'<BR />' +  str(key) + ':' + str(value) for key, value in data['theports'].items()} 
+        # 
 
         nlist = parent_node
         path_color = get_random_hex_color()
+        for level, value in enumerate(parent_node):
+            graph.add_node(value, label=f"IP: {value}", shape='rectangle', row=level)
+        
+        graph.add_node(host, label=f"<IP: {host} \n Ports: \n {formatedPorts}>", shape='rectangle', color=f"{path_color}", row=len(parent_node)-1)    
+        #graph.add_path(list(parent_node))
         if len(nlist) > 1:
             fromv = nlist.pop(0)
             
@@ -89,7 +95,7 @@ def create_network_graph(scanned_hosts, filename="all"):
                 tov = nlist.pop(0)
                 # lastHop[:lastHop.rfind(".")]
                 
-                if len(graph.in_edges((fromv,tov))) < 6:
+                if len(graph.in_edges((fromv,tov))) < 7:
                     if graph.has_edge(fromv,tov, key=f"{fromv}_{tov}") == False:
                         
                         graph.add_edge(fromv, tov, key=f"{fromv}_{tov}_{host}", color=f"{path_color}")
@@ -99,9 +105,18 @@ def create_network_graph(scanned_hosts, filename="all"):
                         graph.add_edge(fromv, tov, key=f"{fromv}_{tov}", penwidth="4", color="blue")
 
                 fromv = tov
-    graph.layout("neato")
+    graph.layout("dot")
     graph.draw(f"graph_{filename}.svg")
 
+def is_up(ip):
+    icmp = IP(dst=ip)/ICMP()
+    resp = sr1(icmp, timeout=1)
+    if resp == None:
+        return False
+    else:
+        return True
+#  def gen_IP_list(ipExpression = "0.0.0.0"):
+     
 
 if __name__ == '__main__':
     scan_dict = read_xml('outputscan.xml')
@@ -109,9 +124,9 @@ if __name__ == '__main__':
     scanned_hosts = get_scanned_hosts(scan_dict)
     uniqueNets = set([ips[:ips.rfind(".")] for ips in scanned_hosts])
     perform_traceroute(scanned_hosts)
-    for un in uniqueNets:
-        #print (un)
-        create_network_graph({k:v for (k,v) in scanned_hosts.items() if un in k},un)
+    # for un in uniqueNets:
+    #     #print (un)
+    #     create_network_graph({k:v for (k,v) in scanned_hosts.items() if un in k},un)
     
 
 
