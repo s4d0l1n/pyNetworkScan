34c967393966de83fbbef48a3809fe6f30d13ca5
s4d0l1n <129994936+s4d0l1n@users.noreply.github.com>
2023-07-06 21:44:46 -0700

    Update pyNetworkScan.py

--- pyNetworkScan.py
+++ pyNetworkScan.py
@@ -1,16 +1,23 @@
 import xmltodict
 import pprint
 from scapy.all import *
-from scapy.layers.inet import IP, ICMP
+from scapy.layers.inet import IP, ICMP, TCP
 from collections import OrderedDict
 import pygraphviz as pgv
 import socket
 import numpy as np
-
+from multiprocessing.pool import Pool
+import multiprocessing
+from tqdm import tqdm
+from functools import partial
 
 thisip=""
 allhops = []
 hostsAndHops = dict()
+
+
+
+
 def read_xml(file_name):
     with open(file_name) as file:
         return xmltodict.parse(file.read())
@@ -22,7 +29,7 @@ def getMyIP():
     s.close()
     return myip
 
-def get_scanned_hosts(scan_dict, net=""):
+def get_xml_scanned_hosts(scan_dict, net=""):
     scanned_hosts = {}
 
     for host in scan_dict['nmaprun']['host']:
@@ -46,29 +53,38 @@ def get_open_ports(ports):
         open_ports[ports['@portid']] = ports['service']['@name']
 
     return open_ports
-
+def tracert(host):
+    ans, unans = sr(IP(dst=host, ttl=(1,6))/ICMP(), timeout=2, verbose=0, retry=2)
+    hops=[]
+    ans.summary( lambda s, r : hops.append(str(r.sprintf("%IP.src%"))))
+    hops = list(dict.fromkeys(hops))
+    hops.insert(0, thisip)
+    hops.insert(-1, host[:host.rfind(".")]+ ".0")
+    return (host,hops)
+        
+ 
 
 def perform_traceroute(scanned_hosts):
-    for host in scanned_hosts.keys():
-        print(f"tracerouting {host}:")
-        ans, unans = sr(IP(dst=host, ttl=(1,6))/ICMP(), timeout=2, verbose=0, retry=2)
-        hops=[]
-        ans.summary( lambda s, r : hops.append(str(r.sprintf("%IP.src%"))))
-        hops = list(dict.fromkeys(hops))
-        hops.insert(0, thisip)
-        hops.insert(-1, host[:host.rfind(".")]+ ".0")
+   
+    with multiprocessing.Pool(processes=4) as p:
+        with tqdm(total=len(scanned_hosts), unit="Traces") as trace_pbar:
+            for x in p.imap_unordered(tracert, scanned_hosts.keys()):
+                # print(f'{x[0]} {x[1]}')
+
+                scanned_hosts[str(x[0])]["parentIP"] = x[1]
+                trace_pbar.update()
+            
+
+
         
-        scanned_hosts[host]["parentIP"] = hops
-        hostsAndHops[host] = hops
-        pprint.pprint(scanned_hosts[host])
-    pprint.pprint(hostsAndHops)
+    
         
 
 def get_random_hex_color():
     color = list(np.random.choice(range(40,225),size=3))
     return '#{:02x}{:02x}{:02x}'.format(color[0], color[1], color[2])
     
-def gen_ip_list(ip_range_str):
+def gen_ip_list(ip_range_str="192.168.0-15.1-150"):
     ip_list = []
 
     octets = ip_range_str.split(".")
@@ -133,28 +149,97 @@ def create_network_graph(scanned_hosts, filename="all"):
 
 def is_up(ip):
     icmp = IP(dst=ip)/ICMP()
-    resp = sr1(icmp, timeout=1)
+    resp = sr1(icmp, timeout=.5, verbose=0)
+    
     if resp == None:
-        return False
+        return (ip, False)
     else:
-        return True
-#  def gen_IP_list(ipExpression = "0.0.0.0"):
-     
+        #print(f"{ip} is UP")
+        return (ip, True)
+
+def get_live_hosts(ipList): #zero
+    ips = set()
+    with multiprocessing.Pool(processes=100) as p:
+        with tqdm(total=len(ipList), unit="Hosts") as pbar:
+            for x in p.imap_unordered(is_up, ipList):
+                if x[1] == True:
+                    pbar.set_description(f"Found {len(ips)} hosts.")
+                    ips.add(x[0])
+                pbar.update()
+    p.close()
+    p.join()
+    return list(ips)
+
+def check_port(hostandport):
+
+    packet = IP(dst=str(hostandport[0]))/TCP(dport=int(hostandport[1]))
+    
+    resp = sr1(packet, verbose=0, timeout=.5)
+    if resp is not None and resp.haslayer(TCP) and resp.getlayer(TCP).flags==0x12:
+        return (hostandport[0], hostandport[1], True)
+    else:
+        return (hostandport[0], hostandport[1], False)
+
+def get_scan_hosts_ports(liveHostList, top_ports=1000): 
+    scanned_hosts = {}
+    
+    for lh in liveHostList:
+        scanned_hosts[str(lh)] = {"theports": {}, "parentIP": []}
+
+    ports = load_ports_file("theports.txt", top_ports)
+    # pprint.pprint(ports)
+    hostports = []
+    for h in liveHostList:
+        for p in ports.keys():
+            hostports.append((h,p))
+    
+    openPorts = {}
+    with multiprocessing.Pool(processes=100) as p:
+        with tqdm(total=len(hostports), unit="Ports", leave=True, position=0) as host_pbar:
+            host_pbar.set_description("Ports scan: ")
+            for x in p.imap_unordered(check_port, hostports):
+                if x[2]==True:
+
+                    scanned_hosts[f'{x[0]}']["theports"][str(x[1])]=ports[f'{x[1]}']
+
+                host_pbar.update()
+    
+    p.close()
+    p.join()
+
+
+    return scanned_hosts
+    
+        
+    
+
+
+def load_ports_file(infile="theports.txt", top_ports=1000): #1st
+    ports = dict()
+    with open(infile, "r") as line:
+        for l in line.readlines()[0:top_ports]:
+            theline = l.strip().split(",")
+            ports[theline[1]] = theline[0]
+    return ports
+
 
 if __name__ == '__main__':
+    scanned_hosts = dict()
+    
+    
     scan_dict = read_xml('outputscan.xml')
     thisip= getMyIP()
-    scanned_hosts = get_scanned_hosts(scan_dict)
+    ##scanned_hosts = get_xml_scanned_hosts(scan_dict)
+    liveHosts = get_live_hosts(gen_ip_list("192.168.0-50.100-150"))
+    scanned_hosts = get_scan_hosts_ports(liveHosts, 200)
+    
     uniqueNets = set([ips[:ips.rfind(".")] for ips in scanned_hosts])
     perform_traceroute(scanned_hosts)
-    # for un in uniqueNets:
-    #     #print (un)
-    #     create_network_graph({k:v for (k,v) in scanned_hosts.items() if un in k},un)
+    for un in uniqueNets:
+        #print (un)
+        create_network_graph({k:v for (k,v) in scanned_hosts.items() if un in k},un)
+    # 192.168.0-50.2-150
     
 
-
-    
-
-    
     create_network_graph(scanned_hosts)
-    #pprint.pprint(allhops)
+    
